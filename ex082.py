e7c9b87405ed9db15e73b0b76a74740c934cbfc0
Ana Clara Aparecida <anaclara.aparecida.010@gmail.com>
2025-05-07 14:04:56 -0300

    exercicios python

--- ex082.py
+++ ex082.py
@@ -1,1 +1,15 @@
 lista = []
+exp = str(input('Digite uma expressão: '))
+for simb in exp:
+    if simb in '(':
+        lista.append('(')
+    elif simb == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão esta valida! ')
+else:
+    print('Sua expressão esta invalida')
